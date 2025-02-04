02ea6a996122a53ee2f55f6aaa8d0929a8e1f087
srinu-vinay-kumar <srinuvinaykumar.narigiri@gmail.com>
2025-02-04 15:34:14 +0530

    fourth commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/FAQ-Accordion",
+  base:"/Frontend-Mentor-Challengess/FAQ-Accordion",
   server: {
     port: 2002
   }
